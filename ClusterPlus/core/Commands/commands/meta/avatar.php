397b68bfc3190bc20cec4fb0fd1d9fd5fb57d921
Animeshz <animeshsahu19@yahoo.com>
2018-09-11 22:08:08 +0530

    [ClusterPlus] Deleted unncessary lines and used c-user type in args

--- ClusterPlus/core/Commands/commands/meta/avatar.php
+++ ClusterPlus/core/Commands/commands/meta/avatar.php
@@ -18,7 +18,7 @@ return function(\CharlotteDunois\Yasmin\Client $client) {
 					array(
 						'key' => 'user',
 						'prompt' => 'user',
-						'type' => 'user',
+						'type' => 'c-user',
 						'default' => ''
 					)
 				)
@@ -30,54 +30,17 @@ return function(\CharlotteDunois\Yasmin\Client $client) {
 			$embed = new \CharlotteDunois\Yasmin\Models\MessageEmbed(['color'=> '3447003']);
 			if($args['user'] !== '') {
 				$av = $args['user']->getAvatarURL();
-				if($av !== null) {
-					$embed->setImage($av);
-				} else {
-					$embed->setDescription('Image not found');
-				}
-				$message->channel->send('', ['embed' => $embed]);
 			} else {
-				$embed->setDescription('mention a user or send name or tag');
-				$message->channel->send('', ['embed' => $embed]);
-
-				$listener = function (\CharlotteDunois\Yasmin\Models\Message $msg) use ($message, &$listener) {
-					if($msg->channel->__toString() === $message->channel->__toString() && $msg->author->__toString() === $message->author->__toString()) {
-
-						$embed = new \CharlotteDunois\Yasmin\Models\MessageEmbed(['color'=> '3447003']);
-						$removeListener = true;
-						if($msg->mentions->members->first() !== null) {
-							$av = $msg->mentions->members->first()->user->getAvatarURL();
-							if($av !== null) {
-								$embed->setImage($av);
-							} else {
-								$embed->setDescription('User found, Image not found');
-							}
-						} else {
-							$found = $this->client->users->filter(function($user) use ($msg) { return (mb_stripos(mb_strtolower($user->tag), mb_strtolower($msg->content))); });
-							$count = $found->count();
-
-							if($count === 0) $embed->setDescription('User not found.');
-
-							if($count === 1) {
-								$av = $found->first()->getAvatarURL();
-								if($av !== null) {
-									$embed->setImage($av);
-								} else {
-									$embed->setDescription('User found, Image not found');
-								}
-							}
-
-							if($count > 1) {
-								$desc = \CharlotteDunois\Livia\Utils\DataHelpers::disambiguation($found, 'users', null).\PHP_EOL;
-								$embed->setDescription($desc);
-								$removeListener = false;
-							}
-						}
-						return $msg->channel->send('', ['embed' => $embed])->done(function() use ($removeListener, &$listener) { ($removeListener === false) ?: $this->client->removeListener('message', $listener); });
-					}
-				};
-				$this->client->on('message', $listener);
+				$av = $message->message->author->getAvatarURL();
 			}
+
+			if($av !== null) {
+				$embed->setImage($av);
+			} else {
+				$embed->setDescription('Image not found');
+			}
+
+			$message->message->channel->send('', ['embed' => $embed]);
 		}
 	});
 };
