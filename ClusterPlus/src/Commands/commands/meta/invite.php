4ea5532906e39357638433e75e9ba2353dd93b5a
Animeshz <animeshsahu19@yahoo.com>
2018-10-21 12:11:43 +0530

    [ClusterPlus] create invite command

---
+++ ClusterPlus/src/Commands/commands/meta/invite.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * registers link to invite the bot to any server
+ * 
+ * @package    ClusterPlus
+ * @author     Animesh Sahu <animeshsahu19@yahoo.com>
+ */
+
+return function ($client) {
+    return (new class($client) extends \CharlotteDunois\Livia\Commands\Command {
+        function __construct(\CharlotteDunois\Livia\LiviaClient $client) {
+            parent::__construct($client, [
+                'name' => 'invite',
+                'group' => 'meta',
+                'description' => 'Makes url to invite bot to any server',
+                'details' => 'Invite is made of OAuth2 api',
+                'examples' => ['invite'],
+                'guarded' => true
+            ]);
+        }
+        
+        function run(\CharlotteDunois\Livia\CommandMessage $message, \ArrayObject $args, bool $fromPattern) {
+        	return $message->client->fetchApplication()->done(
+        		function( $oauth2 ) use ( $message )
+        		{
+        			return $message->say( 'Invite me on your server by https://discordapp.com/oauth2/authorize?client_id=' . $oauth2->id . '&permissions=8&scope=bot' );
+        		}
+        	);
+        }
+    });
+};
