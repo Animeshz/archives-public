1694e9211dfa7d058e17585199e2d3929ea52f37
Animesh Sahu <animeshsahu19@yahoo.com>
2019-03-25 10:49:28 +0530

    [Brave-Project] Argument collector initial stage

---
+++ Brave-Project/src/main/java/lichi/brave/models/Argument.kt
@@ -0,0 +1,19 @@
+package lichi.brave.models
+
+import net.dv8tion.jda.api.JDA
+
+class Argument(val jda: JDA, val arg: Map<String, Any>)
+{
+	fun pull()
+	{
+
+	}
+
+	fun parse(arg: String)
+	{
+		//convert the string object to specified type in arg property
+	}
+
+	fun validate(arg: String)
+	{}
+}
