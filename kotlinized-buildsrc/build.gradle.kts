8a2170cfda47744aeabaca41b4b3151b5e25dd69
Animeshz <animeshsahu19@yahoo,com>
2020-11-04 21:03:27 +0530

    [kotlinized-buildsrc] Initial Commit

---
+++ kotlinized-buildsrc/build.gradle.kts
@@ -0,0 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.github.animeshz"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ kotlinized-buildsrc/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "kotlinized-buildsrc"
+
