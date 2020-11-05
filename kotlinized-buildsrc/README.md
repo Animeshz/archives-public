e0c86f6012cf6e910665b7c608b1bbeebd1a3e01
Animeshz <animeshsahu19@yahoo,com>
2020-11-05 20:36:45 +0530

    [kotlinized-buildsrc] Created .gitignore, added dokka plugin and created docs updater github workflow.

--- kotlinized-buildsrc/README.md
+++ kotlinized-buildsrc/README.md
@@ -1,5 +1,7 @@
 # Kotlinized BuildSrc
 
-A [gradle buildSrc](https://docs.gradle.org/current/userguide/organizing_gradle_projects.html#sec:build_sources) companion ease with the management of Kotlin Gradle Scripts.
+A [gradle buildSrc](https://docs.gradle.org/current/userguide/organizing_gradle_projects.html#sec:build_sources) companion to ease the management of Kotlin Gradle Scripts.
 
 <!-- Explain feature in list with example and further... -->
+
+## Documentation
--- kotlinized-buildsrc/build.gradle.kts
+++ kotlinized-buildsrc/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.ajoberstar.gradle.git.publish.tasks.GitPublishReset
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("org.jetbrains.dokka") version "1.4.10.2"
+    id("org.ajoberstar.git-publish") version "3.0.0"
     `kotlin-dsl`
 }
 
@@ -10,37 +13,67 @@ version = "0.1"
 
 repositories {
     mavenCentral()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    jcenter()
 }
 
 kotlin {
     explicitApi()
 }
 
-val ktlint: Configuration by configurations.creating
+tasks {
+    val ktlint: Configuration by configurations.creating
+    val buildDir = rootProject.buildDir
+    val docsOutputDir = rootDir.resolve("docs")
 
-tasks.create<JavaExec>("diktatCheck") {
-    group = "diktat"
-
-    inputs.files(project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt")))
-    outputs.dir("${project.buildDir}/reports/diktat/")
-
-    description = "Check Kotlin code style."
-    classpath = ktlint
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("src/*/kotlin/**/*.kt")
-
-    outputs.upToDateWhen { false }
-    isIgnoreExitValue = true
-
-    dependencies {
-        ktlint("com.pinterest:ktlint:0.39.0") {
-            exclude("com.pinterest.ktlint", "ktlint-ruleset-standard")
-        }
-
-        ktlint("org.cqfn.diktat:diktat-rules:0.1.2")
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
     }
+
+    getByName<Delete>("clean") {
+        delete(buildDir)
+        delete(docsOutputDir)
+    }
+
+    dokkaHtml.configure {
+        dependsOn(clean)
+        outputDirectory.set(docsOutputDir)
+    }
+
+    getByName<GitPublishReset>("gitPublishReset") {
+        dependsOn(dokkaHtml)
+    }
+
+    create<JavaExec>("diktatCheck") {
+        group = "diktat"
+
+        inputs.files(project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt")))
+        outputs.dir("${project.buildDir}/reports/diktat/")
+
+        description = "Check Kotlin code style."
+        classpath = ktlint
+        main = "com.pinterest.ktlint.Main"
+        args = listOf("src/*/kotlin/**/*.kt")
+
+        outputs.upToDateWhen { false }
+        isIgnoreExitValue = true
+
+        dependencies {
+            ktlint("com.pinterest:ktlint:0.39.0") {
+                exclude("com.pinterest.ktlint", "ktlint-ruleset-standard")
+            }
+
+            ktlint("org.cqfn.diktat:diktat-rules:0.1.2")
+        }
+    }
+}
+
+gitPublish {
+    repoUri.set("https://github.com/Animeshz/kotlinized-buildsrc.git")
+    branch.set("gh-pages")
+
+    contents {
+        from(file(rootDir.resolve("docs")))
+    }
+
+    commitMessage.set("Update Docs")
 }
