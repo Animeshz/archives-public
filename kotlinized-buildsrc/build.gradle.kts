663a8b301e5f3386536cdf1428c225c59bdee848
Animeshz <animeshsahu19@yahoo,com>
2020-11-08 18:10:52 +0530

    [kotlinized-buildsrc] Add bintray publishing plugin and set it up.

--- kotlinized-buildsrc/build.gradle.kts
+++ kotlinized-buildsrc/build.gradle.kts
@@ -1,15 +1,25 @@
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.ajoberstar.gradle.git.publish.tasks.GitPublishReset
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+object Library {
+    const val group = "com.github.animeshz"
+    const val version = "0.1-alpha1"
+    const val description =
+            "Kotlinized BuildSrc is a Gradle BuildSrc companion for Kotlin based Gradle Scripts in a multi-module projects."
+}
 
 plugins {
     kotlin("jvm") version "1.4.10"
     id("org.jetbrains.dokka") version "1.4.10.2"
     id("org.ajoberstar.git-publish") version "3.0.0"
+    id("com.jfrog.bintray") version "1.8.5"
     `kotlin-dsl`
+    `maven-publish`
 }
 
-group = "com.github.animeshz"
-version = "0.1"
+group = Library.group
+version = Library.version
 
 repositories {
     mavenCentral()
@@ -41,6 +51,43 @@ tasks {
 
     getByName<GitPublishReset>("gitPublishReset") {
         dependsOn(dokkaHtml)
+    }
+
+    val sourcesJar by registering(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    configure<BintrayExtension> {
+        user = System.getenv("BINTRAY_USER")
+        key = System.getenv("BINTRAY_KEY")
+
+        pkg = PackageConfig().apply {
+            repo = "maven"
+            name = "kotlinized-buildsrc"
+            setLicenses("MIT")
+            vcsUrl = "https://github.com/Animeshz/kotlinized-buildsrc.git"
+            issueTrackerUrl = "https://github.com/Animeshz/kotlinized-buildsrc/issues"
+
+            version = VersionConfig().apply {
+                name = Library.version
+                desc = Library.description
+                vcsTag = Library.version
+            }
+        }
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            register("kotlinized-buildsrc", MavenPublication::class) {
+                from(getComponents()["kotlin"])
+                groupId = Library.group
+                artifactId = project.name
+                version = Library.version
+
+                artifact(sourcesJar.get())
+            }
+        }
     }
 
     create<JavaExec>("diktatCheck") {
