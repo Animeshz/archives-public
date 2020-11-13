a97a4ca56050e9c22130832f55427b888b683eb8
Animeshz <animeshsahu19@yahoo,com>
2020-11-13 14:15:07 +0530

    [kotlin-benchmarking] Fix all the format issues following diktat code style rules.

--- kotlin-benchmarking/src/main/kotlin/com/github/animeshz/kotlinbenchmarking/ConsoleInput.kt
+++ kotlin-benchmarking/src/main/kotlin/com/github/animeshz/kotlinbenchmarking/ConsoleInput.kt
@@ -1,6 +1,5 @@
 /**
  * This benchmarks the different types of input reading techniques used in Kotlin/JVM.
- *
  * In my current setup (AMD Ryzen 5 4500u | 8GB LPDDR4 3200MHz), following are the test results:
  *
  * Enter amount of lines to be tested: 50000
@@ -10,11 +9,38 @@
  * readUsingBufferedReader(): 17 ms
  */
 
-package com.github.animeshz.kotlin.benchmarking
+package com.github.animeshz.kotlinbenchmarking
 
-import java.io.*
-import java.util.*
-import kotlin.system.*
+import java.io.File
+import java.util.Scanner
+import java.util.UUID
+import kotlin.system.measureTimeMillis
+
+private fun readUsingReadLine(lines: Int) =
+        repeat(lines) {
+            readLine()
+        }
+
+private fun readUsingScanner(lines: Int) =
+        Scanner(System.`in`).use { scanner ->
+            repeat(lines) {
+                scanner.nextLine()
+            }
+        }
+
+private fun readUsingBufferedReader(lines: Int) =
+        System.`in`.bufferedReader().use { reader ->
+            repeat(lines) {
+                reader.readLine()
+            }
+        }
+
+private fun createRandomString(lines: Int): String =
+        buildString {
+            repeat(lines) {
+                appendLine(UUID.randomUUID().toString())
+            }
+        }
 
 fun main() {
     val file = File("testing_files/console_input.txt")
@@ -45,26 +71,4 @@ fun main() {
     println("readUsingReadLine(): $time1 ms")
     println("readUsingScanner(): $time2 ms")
     println("readUsingBufferedReader(): $time3 ms")
-}
-
-private fun readUsingReadLine(lines: Int) {
-    for (i in 0 until lines) readLine()
-}
-
-private fun readUsingScanner(lines: Int) {
-    val sc = Scanner(System.`in`)
-    for (i in 0 until lines) sc.nextLine()
-}
-
-private fun readUsingBufferedReader(lines: Int) {
-    val reader = System.`in`.bufferedReader()
-    for (i in 0 until lines) reader.readLine()
-}
-
-private fun createRandomString(lines: Int): String {
-    val sb = StringBuilder()
-    for (i in 0 until lines) {
-        sb.appendLine(UUID.randomUUID().toString())
-    }
-    return sb.toString()
 }
--- kotlin-benchmarking/src/main/kotlin/com/github/animeshz/kotlinbenchmarking/Util.kt
+++ kotlin-benchmarking/src/main/kotlin/com/github/animeshz/kotlinbenchmarking/Util.kt
@@ -1,30 +1,34 @@
-package com.github.animeshz.kotlin.benchmarking
+/**
+ * This file contains utilities used by the benchmarks.
+ */
 
-import java.io.*
+package com.github.animeshz.kotlinbenchmarking
+
+import java.io.File
 
 /**
  * Writes to file with given [fileName], each element of [collection] in each line.
- * Creates file if not exists
+ * Creates file if not exists.
  */
 fun writeToFile(fileName: String, collection: Collection<*>) =
-    writeToFile(File(fileName), collection)
+        writeToFile(File(fileName), collection)
 
 /**
  * Writes to [file], each element of [collection] in each line.
- * Creates file if not exists
+ * Creates file if not exists.
  */
-fun writeToFile(file: File, collection: Collection<*>) {
-    file.createNewFile()
-    file.printWriter().apply {
-        collection.forEach {
-            println(it)
+fun writeToFile(file: File, collection: Collection<*>) = file
+        .apply { createNewFile() }
+        .printWriter()
+        .apply {
+            collection.forEach {
+                println(it)
+            }
         }
-    }.close()
-}
+        .close()
 
 /**
- * Builds [String] using [StringBuilder], applying operation from [block]
+ * Builds [String] using [StringBuilder], applying operation from [block].
  */
-inline fun buildString(block: StringBuilder.() -> Unit): String {
-    return StringBuilder().apply(block).toString()
-}
+inline fun buildString(block: StringBuilder.() -> Unit): String =
+        StringBuilder().apply(block).toString()
