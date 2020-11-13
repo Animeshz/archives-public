9391a808e24f14e9232f37747ff880a930c73d7f
Animeshz <animeshsahu19@yahoo,com>
2020-11-13 13:41:39 +0530

    [kotlin-benchmarking] Add diktat (strict code analyser), and create new benchmark: ConsoleInput.

--- kotlin-benchmarking/build.gradle.kts
+++ kotlin-benchmarking/build.gradle.kts
@@ -1,12 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+object Versions {
+    const val ktlint = "0.39.0"
+    const val diktat = "0.1.3"
+}
+
 plugins {
-    kotlin("jvm") version "1.4-M1"
+    kotlin("jvm") version "1.4.10"
 }
 
 group = "com.github.animeshz"
 version = "0.0.1"
 
 repositories {
-    maven("https://dl.bintray.com/kotlin/kotlin-eap")
     mavenCentral()
 }
 
@@ -15,10 +21,42 @@ dependencies {
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+    val ktlint: Configuration by configurations.creating
+    val diktatConfig: JavaExec.() -> Unit = {
+        group = "diktat"
+        classpath = ktlint
+        main = "com.pinterest.ktlint.Main"
+
+        inputs.files(project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt")))
+        outputs.dir("${project.buildDir}/reports/diktat/")
+
+        outputs.upToDateWhen { false }  // Allows to run the task again (otherwise skipped till sources are changed).
+        isIgnoreExitValue = true  // Allows to skip the non-zero exit code, can be useful when other tasks depends on linter
+
+        dependencies {
+            ktlint("com.pinterest:ktlint:${Versions.ktlint}") {
+                exclude("com.pinterest.ktlint", "ktlint-ruleset-standard")
+            }
+
+            ktlint("org.cqfn.diktat:diktat-rules:${Versions.diktat}")
+        }
     }
-    compileTestKotlin {
+
+    create<JavaExec>("diktatCheck") {
+        diktatConfig()
+        description = "Check Kotlin code style."
+
+        args = listOf("src/*/kotlin/**/*.kt")    // specify proper path to sources that should be checked here
+    }
+
+    create<JavaExec>("diktatFormat") {
+        diktatConfig()
+        description = "Fix Kotlin code style deviations."
+
+        args = listOf("-F", "src/main/kotlin/**/*.kt")  // specify proper path to sources that should be checked here
+    }
+
+    withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
 }
---
+++ kotlin-benchmarking/src/main/kotlin/com/github/animeshz/kotlin/benchmarking/ConsoleInput.kt
@@ -0,0 +1,67 @@
+package com.github.animeshz.kotlin.benchmarking
+
+import java.io.*
+import java.util.*
+import kotlin.system.*
+
+private fun readUsingReadLine(lines: Int) {
+    for (i in 0 until lines) readLine()
+}
+
+private fun readUsingScanner(lines: Int) {
+    val sc = Scanner(System.`in`)
+    for (i in 0 until lines) sc.nextLine()
+}
+
+private fun readUsingBufferedReader(lines: Int) {
+    val reader = System.`in`.bufferedReader()
+    for (i in 0 until lines) reader.readLine()
+}
+
+private fun createRandomString(lines: Int): String {
+    val sb = StringBuilder()
+    for (i in 0 until lines) {
+        sb.appendLine(UUID.randomUUID().toString())
+    }
+    return sb.toString()
+}
+
+/**
+ * In my current setup (AMD Ryzen 5 4500u | 8GB LPDDR4 3200MHz)
+ *
+ * Enter amount of lines to be tested: 50000
+ * For 50000 lines:
+ * readUsingReadLine(): 4569 ms
+ * readUsingScanner(): 143 ms
+ * readUsingBufferedReader(): 17 ms
+ */
+fun main() {
+    val file = File("testing_files/console_input.txt")
+
+    print("Enter amount of lines to be tested: ")
+    val totalTestingLines = readLine()!!.toInt()
+
+    file.writeText(createRandomString(totalTestingLines))
+
+    val ipStream = file.inputStream()
+    System.setIn(ipStream)
+
+    val time1 = measureTimeMillis {
+        readUsingReadLine(totalTestingLines)
+    }
+
+    ipStream.channel.position(0)
+    val time2 = measureTimeMillis {
+        readUsingScanner(totalTestingLines)
+    }
+
+    ipStream.channel.position(0)
+    val time3 = measureTimeMillis {
+        readUsingBufferedReader(totalTestingLines)
+    }
+
+    println("For $totalTestingLines lines:")
+    println("readUsingReadLine(): $time1 ms")
+    println("readUsingScanner(): $time2 ms")
+    println("readUsingBufferedReader(): $time3 ms")
+}
