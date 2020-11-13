aeedf75e808aa588e315bc1f6d72ecf144f4bbfa
Animeshz <animeshsahu19@yahoo,com>
2020-11-13 14:13:58 +0530

    [kotlin-benchmarking] Removed .idea directory from repository.

--- kotlin-benchmarking/src/main/kotlin/com/github/animeshz/kotlinbenchmarking/ConsoleInput.kt
+++ kotlin-benchmarking/src/main/kotlin/com/github/animeshz/kotlinbenchmarking/ConsoleInput.kt
@@ -1,33 +1,7 @@
-package com.github.animeshz.kotlin.benchmarking
-
-import java.io.*
-import java.util.*
-import kotlin.system.*
-
-private fun readUsingReadLine(lines: Int) {
-    for (i in 0 until lines) readLine()
-}
-
-private fun readUsingScanner(lines: Int) {
-    val sc = Scanner(System.`in`)
-    for (i in 0 until lines) sc.nextLine()
-}
-
-private fun readUsingBufferedReader(lines: Int) {
-    val reader = System.`in`.bufferedReader()
-    for (i in 0 until lines) reader.readLine()
-}
-
-private fun createRandomString(lines: Int): String {
-    val sb = StringBuilder()
-    for (i in 0 until lines) {
-        sb.appendLine(UUID.randomUUID().toString())
-    }
-    return sb.toString()
-}
-
 /**
- * In my current setup (AMD Ryzen 5 4500u | 8GB LPDDR4 3200MHz)
+ * This benchmarks the different types of input reading techniques used in Kotlin/JVM.
+ *
+ * In my current setup (AMD Ryzen 5 4500u | 8GB LPDDR4 3200MHz), following are the test results:
  *
  * Enter amount of lines to be tested: 50000
  * For 50000 lines:
@@ -35,6 +9,13 @@ private fun createRandomString(lines: Int): String {
  * readUsingScanner(): 143 ms
  * readUsingBufferedReader(): 17 ms
  */
+
+package com.github.animeshz.kotlin.benchmarking
+
+import java.io.*
+import java.util.*
+import kotlin.system.*
+
 fun main() {
     val file = File("testing_files/console_input.txt")
 
@@ -64,4 +45,26 @@ fun main() {
     println("readUsingReadLine(): $time1 ms")
     println("readUsingScanner(): $time2 ms")
     println("readUsingBufferedReader(): $time3 ms")
+}
+
+private fun readUsingReadLine(lines: Int) {
+    for (i in 0 until lines) readLine()
+}
+
+private fun readUsingScanner(lines: Int) {
+    val sc = Scanner(System.`in`)
+    for (i in 0 until lines) sc.nextLine()
+}
+
+private fun readUsingBufferedReader(lines: Int) {
+    val reader = System.`in`.bufferedReader()
+    for (i in 0 until lines) reader.readLine()
+}
+
+private fun createRandomString(lines: Int): String {
+    val sb = StringBuilder()
+    for (i in 0 until lines) {
+        sb.appendLine(UUID.randomUUID().toString())
+    }
+    return sb.toString()
 }
