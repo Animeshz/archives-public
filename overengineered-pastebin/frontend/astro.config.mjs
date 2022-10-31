3fb596d6f92d52a5dee53389ca220e577a166ddc
aadi58002 <aadi58002gmail.com>
2022-10-31 21:55:46 +0530

    [overengineered-pastebin] Initail Commit

---
+++ overengineered-pastebin/frontend/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue(), tailwind()]
+});
